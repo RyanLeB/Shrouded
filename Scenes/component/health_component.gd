586618df2a5d2fbfbe48deda6fac6346907e8a94
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-02-25 13:03:07 -0400

    health system

---
+++ Scenes/component/health_component.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name HealthComponent
+
+signal died
+
+@export var max_health: float = 10
+var current_health
+
+func _ready():
+	current_health = max_health
+
+
+func damage(damage: float):
+	current_health = max(current_health - damage, 0)
+	if current_health == 0:
+		died.emit()
+		owner.queue_free()
