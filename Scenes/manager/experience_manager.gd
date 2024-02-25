381930583123690fc13d93e02037c215dace0e9c
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-02-25 15:31:49 -0400

    xp bar working

--- Scenes/manager/experience_manager.gd
+++ Scenes/manager/experience_manager.gd
@@ -1,14 +1,25 @@
 extends Node
 
+signal experience_updated(current_experience: float, target_experience: float)
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_experience = 0
+var current_level = 1
+var target_experience = 5
 
 func _ready():
 	GameEvent.experience_vial_collected.connect(on_experience_vial_collected)
 
 
 func increment_experience(number: float):
-	current_experience += number
-	print(current_experience)
+	current_experience = min(current_experience + number, target_experience)
+	experience_updated.emit(current_experience, target_experience)
+	if current_experience == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		experience_updated.emit(current_experience, target_experience)
 
 
 func on_experience_vial_collected(number: float):
