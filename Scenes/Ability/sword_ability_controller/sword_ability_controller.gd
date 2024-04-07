b60805b1df9a26f2ff5531d6287e24d5ac6e33e5
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-04-07 13:53:56 -0300

    sword rate upgrade working

--- Scenes/Ability/sword_ability_controller/sword_ability_controller.gd
+++ Scenes/Ability/sword_ability_controller/sword_ability_controller.gd
@@ -3,13 +3,15 @@ extends Node
 const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
-var damage = 5
 
+var damage = 5
+var base_wait_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
-
+	GameEvent.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -42,3 +44,14 @@ func on_timer_timeout():
 	
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+	
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
+	
+	print($Timer.wait_time)
