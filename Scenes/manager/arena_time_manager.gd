56ed34c933d925b6cad34739b8026f854182efdf
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-04-23 09:55:13 -0300

    victory screen functional

--- Scenes/manager/arena_time_manager.gd
+++ Scenes/manager/arena_time_manager.gd
@@ -1,8 +1,17 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
 
 @onready var timer = $Timer
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
 
 
 func get_time_elapsed():
 	return $Timer.wait_time - $Timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
