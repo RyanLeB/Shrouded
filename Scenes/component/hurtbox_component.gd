9391c4c5a7c4fb04590b939dba26140fa06693ca
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-04-24 13:55:31 -0300

    damage numbers, font added

--- Scenes/component/hurtbox_component.gd
+++ Scenes/component/hurtbox_component.gd
@@ -3,6 +3,9 @@ class_name HurtboxComponent
 
 @export var health_component: Node
 
+# preloaded floating text
+var floating_text_scene = preload("res://Scenes/ui/floating_text.tscn")
+
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -17,3 +20,9 @@ func on_area_entered(other_area: Area2D):
 	
 	var hitbox_component = other_area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
