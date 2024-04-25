685424185e248fd9329c1fe95a69e2b06fe5ec04
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-04-25 11:51:02 -0300

    End screens styled

--- Scenes/ui/end_screen.gd
+++ Scenes/ui/end_screen.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+@onready var panel_container = $%PanelContainer
+
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+	panel_container.scale = Vector2.ZERO
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	$%RestartButton.pressed.connect(on_restart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
