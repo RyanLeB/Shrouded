a92cb23c9d8d938e998f9f3442514c766b32b4ae
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-04-30 11:48:43 -0300

    Addons implemented, background music player working

--- Scenes/ui/end_screen.gd
+++ Scenes/ui/end_screen.gd
@@ -19,6 +19,16 @@ func _ready():
 func set_defeat():
 	$%TitleLabel.text = "Defeat"
 	$%DescriptionLabel.text = "You Lost!"
+	play_jingle(true)
+	
+	
+
+func play_jingle(defeat: bool = false):
+	if defeat:
+		$DefeatStreamPlayer.play()
+	else:
+		$VictoryStreamPlayer.play()
+
 
 func on_restart_button_pressed():
 	get_tree().paused = false
